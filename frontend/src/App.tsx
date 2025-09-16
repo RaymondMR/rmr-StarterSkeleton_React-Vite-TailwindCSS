627b6a351f47265880fa2ea5e0cf6877091d0896
RaymondMR <ramonmontoya@outlook.com>
2025-09-15 20:38:15 -0600

    "Se ha creado el Skeleton para React-Vite-Tailwindcss-Typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+
+const App=()=>{
+return (
+  <div className="Container bg-red-500">
+    <div className="Container flex justify-center-safe p-8 font-[1000]">Tailwindcss integrated succesfully </div>
+  </div>
+)
+}
+
+export default App
